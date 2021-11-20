40eacdfcb21e1eaeb253d9350814f73f12a12751
Kenta Taoka <43945931+kentoak@users.noreply.github.com>
2021-11-20 23:25:56 +0900

    Add files via upload

---
+++ deepl.js
@@ -0,0 +1,26 @@
+function jumpTextSearch() {
+	return function(info, tab) {
+		var str = info.selectionText;
+		var url = 'https://www.deepl.com/translator#en/ja/'+encodeURIComponent(str.replace(/[\/\\\|]/g,"\\$&"))+'';
+		//ここ田岡
+		chrome.windows.create({
+			url: url,
+			type: 'popup',
+			top: 5,
+            left: 0,
+			width: Math.round(screen.availWidth/2), 
+			height: Math.round(screen.availHeight/2),
+			focused: true,
+		});
+		//window.open(url,null);
+	};
+};
+
+var parentId = chrome.contextMenus.create({
+	//"title" : chrome.i18n.getMessage('extContextmenu'),
+	"title" : "D_DeepL",
+	"type" : "normal",
+	"contexts" : ["selection"],
+	"onclick" : jumpTextSearch()
+});
+
